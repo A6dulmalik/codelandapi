b5c6dfc8fcbe2595c079f5f4682f47b69a857140
Abdulmalik A. <amalikabdulmalik04@gmail.com>
2024-12-10 09:41:31 +0100

    edited postType enum

--- src/dto/createPost.dto.ts
+++ src/dto/createPost.dto.ts
@@ -1,1 +1,36 @@
-export class CreatePostDto {}
+import { PostStatus } from "src/post/enums/postStatus.enum";
+import { CreateUserDto } from "./create-user.dto";
+import { IsArray, IsDate, IsEnum,  IsISO8601,  isISO8601,  IsNotEmpty,  IsObject, IsOptional, IsString, MinLength } from "class-validator";
+import { PostType } from "src/post/enums/postType.enum";
+
+export class CreatePostDto {
+    @IsObject()
+    author: CreateUserDto
+    
+    @IsString()
+    @MinLength(4)
+    @IsNotEmpty()
+    title: string;
+
+    @IsEnum(PostType)
+    postType: PostType;
+
+    @IsEnum(PostStatus)
+    postStatus: PostStatus;
+
+    @IsString()
+    @IsNotEmpty()
+    content: string;
+
+    @IsString()
+    @IsOptional()
+    imageUrl: string;
+
+    @IsDate()
+    @IsISO8601()
+    publishedDate: Date;
+
+    @IsArray()
+    @IsString({each: true})
+    tags: string[];
+}
